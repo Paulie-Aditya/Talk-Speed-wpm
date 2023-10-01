502f259738ee22fec9f05fa2f4e817918f219977
Paulie-Aditya <paul.aditya304@gmail.com>
2023-10-01 23:07:35 +0530

    final commit before submission

---
+++ api_communication.py
@@ -0,0 +1,71 @@
+import requests
+from api_secrets import API_KEY_ASSEMBLYAI
+import time
+
+
+# upload
+upload_endpoint = "https://api.assemblyai.com/v2/upload"
+transcript_endpoint = "https://api.assemblyai.com/v2/transcript"
+
+headers = {'authorization': API_KEY_ASSEMBLYAI}
+
+def upload(filename):
+    def read_file(filename, chunk_size = 5242880):
+        with open(filename,"rb") as _file:
+            while True:
+                data = _file.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+    
+    upload_response = requests.post(upload_endpoint,
+                            headers=headers,
+                            data = read_file(filename=filename))
+
+    #print(upload_response.json())
+
+    audio_url = upload_response.json()['upload_url']
+    return audio_url
+
+
+# transcribe
+def transcribe(audio_url):
+    transcribe_request = {"audio_url": audio_url}
+    transcribe_response = requests.post(transcript_endpoint, json=transcribe_request, headers=headers)
+    #print(transcribe_response.json())
+    job_id = transcribe_response.json()['id']
+    return job_id
+
+
+
+# pull
+def pull(transcript_id):
+    pulling_endpoint = transcript_endpoint + '/' + transcript_id
+    pulling_response = requests.get(pulling_endpoint, headers=headers)
+    return pulling_response.json()
+
+def get_transcription_result_url(audio_url):
+    transcript_id = transcribe(audio_url)
+    while True:
+        data = pull(transcript_id)
+        if data['status'] == 'completed':
+            return data,None
+        elif data['status'] == 'error':
+            return data,data["error"]
+        print('Waiting 30 seconds....')
+        time.sleep(30)
+
+
+# save transcript
+def save_transcript(audio_url,filename):
+    data, error = get_transcription_result_url(audio_url)
+    if not error:
+        text_filename = filename.rstrip(".wav")+".txt"
+        with open(text_filename,"w") as f:
+            f.write(data["text"])
+        print("Transcription Saved!")
+        return len(data["text"])
+    elif error:
+        print("Error!!", error)
+        return 0
---
+++ main.py
@@ -0,0 +1,24 @@
+# Talk Speed Research
+
+import wave
+from api_communication import *
+
+filename = "fight_club.wav"
+
+obj = wave.open(filename,"rb")
+
+sample_freq = obj.getframerate()
+n_samples = obj.getnframes()
+obj.close()
+
+t_audio =  n_samples/sample_freq  # total time in seconds.
+
+print(f'Length of Audio: {t_audio}')
+
+audio_url = upload(filename)
+number_of_words = save_transcript(audio_url=audio_url, filename=filename)
+
+print(f"Words per minute: {int((number_of_words/t_audio)*60)}")
+
+
+
